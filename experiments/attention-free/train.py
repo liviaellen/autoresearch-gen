530422d74ceca28bf23e082675de8a37156ebcf8
Livia Ellen <liviaellen@msn.com>
2026-03-13 15:51:19 -0700

    Exp122: warmdown=0.65

--- experiments/attention-free/train.py
+++ experiments/attention-free/train.py
@@ -84,7 +84,7 @@ class GPT(nn.Module):
         self.config = config
         self.wte = nn.Embedding(config.vocab_size, config.n_embd)
         # Multi-scale kernels for speed/context tradeoff
-        kernel_sizes = [3, 7, 15, 31, 63, 127, 255, 31][:config.n_layer]
+        kernel_sizes = [3, 7, 15, 31, 63, 127, 15, 31][:config.n_layer]
         self.blocks = [Block(config, kernel_size=kernel_sizes[i]) for i in range(config.n_layer)]
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
 
@@ -260,7 +260,7 @@ SCALAR_LR = 0.5
 WEIGHT_DECAY = 0.1
 ADAM_BETAS = (0.8, 0.95)
 WARMUP_RATIO = 0.25
-WARMDOWN_RATIO = 0.7
+WARMDOWN_RATIO = 0.65
 FINAL_LR_FRAC = 0.0
 
 DEPTH = 8
