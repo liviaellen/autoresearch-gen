ee37ce5bed16c210baf9b6f483359f610d180cf6
Livia Ellen <liviaellen@msn.com>
2026-03-13 10:57:21 -0700

    Exp78: conv LR = 3x matrix_lr

--- experiments/attention-free/train.py
+++ experiments/attention-free/train.py
@@ -159,7 +159,7 @@ class AdamW:
                 }
             elif "conv" in path:
                 self.param_config[path] = {
-                    "lr": matrix_lr * 2.0, "betas": adam_betas,
+                    "lr": matrix_lr * 3.0, "betas": adam_betas,
                     "eps": 1e-10, "weight_decay": 0.0,
                 }
             elif "wte" in path:
